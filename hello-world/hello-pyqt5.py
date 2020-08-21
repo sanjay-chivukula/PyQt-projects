9da6de9664b8ada2e4d935c4294ccd4de3f83f13
Sanjay Chivukula <sanjay.chivukula@gmail.com>
2020-08-21 10:27:30 -0400

    Modify hello-world/ proj: basic version of pyqt5 window.

--- hello-world/hello-pyqt5.py
+++ hello-world/hello-pyqt5.py
@@ -3,21 +3,21 @@ import sys  # Needed to handle exit status of application.
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QMainWindow
 
-app = QApplication(sys.argv)  # Create this before using any other GUI objects.
 
-# Creating and configuring the window.
-window = QWidget()  # Base class for all UI objects in PyQt.
-window.setWindowTitle("PyQt5 App")
-window.setGeometry(100, 100, 400, 400)
-window.move(10, 10)
+class MainWindow(QMainWindow):
+    pass
 
-# Message to display.
-hello_message = QLabel("<h1>Hello World!</h1>", parent=window)
-hello_message.move(10, 10)
 
-# Displaying the window.
-window.show()
+def test_app_driver(argv=None):
+    app = QApplication(argv)
 
-# Exiting the application.
-sys.exit(app.exec_())
+    window = MainWindow()
+    window.show()
+
+    app.exec_()
+
+
+if __name__ == "__main__":
+    test_app_driver(sys.argv)
