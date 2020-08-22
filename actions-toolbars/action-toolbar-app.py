60934cf00a6fc0b91ab2f27e52591e397d5c8c54
Sanjay Chivukula <sanjay.chivukula@gmail.com>
2020-08-22 11:41:27 -0400

    Update actions-toolbars/ project.

---
+++ actions-toolbars/action-toolbar-app.py
@@ -0,0 +1,28 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtCore import Qt
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("Actions Toolbar App")
+        label = QLabel("Sample Label Content")
+        label.setAlignment(Qt.AlignCenter)
+        self.setCentralWidget(label)
+
+
+def test_app_driver(*args, **kwargs):
+    app = QApplication(*args, **kwargs)
+    window = MainWindow()
+    window.show()
+    app.exec_()
+
+
+if __name__ == "__main__":
+    test_app_driver(sys.argv)
