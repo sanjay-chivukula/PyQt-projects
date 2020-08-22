f051505089574d2ba0847443623297f21e4573fa
Sanjay Chivukula <sanjay.chivukula@gmail.com>
2020-08-22 11:47:13 -0400

    Update actions-toolbars/ project: added code to display toolbar.

--- actions-toolbars/action-toolbar-app.py
+++ actions-toolbars/action-toolbar-app.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QToolBar
 from PyQt5.QtCore import Qt
 
 
@@ -12,9 +13,13 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
 
         self.setWindowTitle("Actions Toolbar App")
+
         label = QLabel("Sample Label Content")
         label.setAlignment(Qt.AlignCenter)
         self.setCentralWidget(label)
+
+        toolbar = QToolBar("Main Toolbar")
+        self.addToolBar(toolbar)
 
 
 def test_app_driver(*args, **kwargs):
